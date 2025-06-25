87fabc501bc37a31417f4657c1b4395587d562c7
TaewoongR <iwoong98@kau.kr>
2025-06-25 11:05:21 +0900

    feat: Activity → NavHost 진입점 설정 (#6)

--- presentation/main/src/main/java/com/yapp/breake/presentation/main/MainActivity.kt
+++ presentation/main/src/main/java/com/yapp/breake/presentation/main/MainActivity.kt
@@ -1,7 +1,32 @@
 package com.yapp.breake.presentation.main
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.MaterialTheme
+import com.yapp.breake.presentation.main.navigation.MainNavigator
+import com.yapp.breake.presentation.main.navigation.rememberMainNavigator
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity()
+class MainActivity : ComponentActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+
+		enableEdgeToEdge()
+
+		setContent {
+			val navigator: MainNavigator = rememberMainNavigator()
+
+			MaterialTheme {
+				MainScreen(
+					navigator = navigator,
+					// TODO : 다크모드 대비 설정
+					onChangeDarkTheme = { false },
+				)
+			}
+		}
+	}
+}
