1a3f18cae7721c5d1503e3308a7ebb20bbce719d
Jun <hky08067@naver.com>
2025-07-04 21:08:44 +0900

    feat: 쿨다운 화면 구현 (#4)

--- presentation/overlay/src/main/java/com/yapp/breake/presentation/overlay/screen/BlockingScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.yapp.breake.presentation.overlay.screen
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-@Composable
-internal fun BlockingScreen(modifier: Modifier = Modifier) {
-
-}
