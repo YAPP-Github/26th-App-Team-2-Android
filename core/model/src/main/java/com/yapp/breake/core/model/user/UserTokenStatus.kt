5e6518043e448f24057d2401d1f16b420f848d8a
TaewoongR <iwoong98@kau.kr>
2025-07-04 20:33:29 +0900

    feat: UserToken model, Mapper 설정 (#12)

---
+++ core/model/src/main/java/com/yapp/breake/core/model/user/UserTokenStatus.kt
@@ -0,0 +1,7 @@
+package com.yapp.breake.core.model.user
+
+enum class UserTokenStatus {
+	ACTIVE,
+	HALF_SIGNUP,
+	INACTIVE,
+}
---
+++ data/src/main/java/com/yapp/breake/data/repository/mapper/LoginTokenMapper.kt
@@ -0,0 +1,15 @@
+package com.yapp.breake.data.repository.mapper
+
+import com.yapp.breake.core.model.user.UserToken
+import com.yapp.breake.core.model.user.UserTokenStatus
+import com.yapp.breake.data.api.model.LoginResponse
+
+internal fun LoginResponse.toData(): UserToken = UserToken(
+	accessToken = accessToken,
+	refreshToken = refreshToken,
+	status = when (memberState) {
+		"ACTIVE" -> UserTokenStatus.ACTIVE
+		"INACTIVE" -> UserTokenStatus.INACTIVE
+		else -> UserTokenStatus.HALF_SIGNUP
+	},
+)
