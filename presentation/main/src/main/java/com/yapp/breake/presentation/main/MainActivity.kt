b4cf86cfe0fe6c17286078ebe49eb66603eea159
TaewoongR <iwoong98@kau.kr>
2025-07-09 01:56:32 +0900

    refactor: 화면 패딩 조정 (#12)

--- presentation/main/src/main/java/com/yapp/breake/presentation/main/MainActivity.kt
+++ presentation/main/src/main/java/com/yapp/breake/presentation/main/MainActivity.kt
@@ -3,7 +3,7 @@ package com.yapp.breake.presentation.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
 import com.yapp.breake.core.designsystem.theme.BrakeTheme
 import com.yapp.breake.presentation.main.navigation.MainNavigator
 import com.yapp.breake.presentation.main.navigation.rememberMainNavigator
@@ -15,7 +15,7 @@ class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 
-		enableEdgeToEdge()
+		WindowCompat.setDecorFitsSystemWindows(window, true)
 
 		setContent {
 			val navigator: MainNavigator = rememberMainNavigator()
