e5dfedcc863a47c29b2cc26530ab1f3ebbfb1773
TaewoongR <iwoong98@kau.kr>
2025-07-10 20:35:53 +0900

    fix: 메인 화면 systembar 패딩 재설정 (#12)

--- presentation/main/src/main/java/com/yapp/breake/presentation/main/MainActivity.kt
+++ presentation/main/src/main/java/com/yapp/breake/presentation/main/MainActivity.kt
@@ -3,7 +3,7 @@ package com.yapp.breake.presentation.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.view.WindowCompat
+import androidx.activity.enableEdgeToEdge
 import com.yapp.breake.core.designsystem.theme.BrakeTheme
 import com.yapp.breake.presentation.main.navigation.MainNavigator
 import com.yapp.breake.presentation.main.navigation.rememberMainNavigator
@@ -15,7 +15,7 @@ class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 
-		WindowCompat.setDecorFitsSystemWindows(window, true)
+		enableEdgeToEdge()
 
 		setContent {
 			val navigator: MainNavigator = rememberMainNavigator()
