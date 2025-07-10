0d910470a810937a73b8831014dee13426a261c6
TaewoongR <iwoong98@kau.kr>
2025-07-11 01:48:03 +0900

    refactor: Activity 에서 Navigation CompositionLocal 전파 (#14)

--- presentation/main/src/main/java/com/yapp/breake/presentation/main/MainActivity.kt
+++ presentation/main/src/main/java/com/yapp/breake/presentation/main/MainActivity.kt
@@ -4,10 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import com.yapp.breake.core.designsystem.theme.BrakeTheme
+import com.yapp.breake.core.navigation.action.MainAction
+import com.yapp.breake.core.navigation.compositionlocal.LocalMainAction
+import com.yapp.breake.core.navigation.compositionlocal.LocalNavigatorAction
 import com.yapp.breake.presentation.main.navigation.MainNavigator
 import com.yapp.breake.presentation.main.navigation.rememberMainNavigator
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import java.net.UnknownHostException
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -19,12 +29,33 @@ class MainActivity : ComponentActivity() {
 
 		setContent {
 			val navigator: MainNavigator = rememberMainNavigator()
+			val coroutineScope: CoroutineScope = rememberCoroutineScope()
+			val snackBarHostState = remember { SnackbarHostState() }
 
-			BrakeTheme {
-				MainScreen(
-					navigator = navigator,
-					onChangeDarkTheme = { false },
-				)
+			val mainAction = object : MainAction {
+				override fun onFinish() = finish()
+				override fun onShowSnackBar(throwable: Throwable?): (Throwable?) -> Unit = { throwable ->
+					coroutineScope.launch {
+						snackBarHostState.showSnackbar(
+							when (throwable) {
+								is UnknownHostException -> getString(R.string.network_error)
+								else -> getString(R.string.unknown_error)
+							},
+						)
+					}
+				}
+			}
+
+			CompositionLocalProvider(
+				LocalMainAction provides mainAction,
+				LocalNavigatorAction provides navigator.navigatorAction(),
+			) {
+				BrakeTheme {
+					MainScreen(
+						navigator = navigator,
+						onChangeDarkTheme = { false },
+					)
+				}
 			}
 		}
 	}
