ada1a646e5a05d1fd993332a28990653962998d3
TaewoongR <iwoong98@kau.kr>
2025-07-15 01:05:55 +0900

    refactor: 로그인 UI, 비지니스 로직 리팩토링 (#17)
    - UI 상태 선언
    - UI 상태에 따른 화면 상태 분리
    - 새로 적용한 웹뷰 로그인 화면과 연결
    - 실질 서버와 연결

--- presentation/login/src/main/java/com/yapp/breake/presentation/login/LoginScreen.kt
+++ presentation/login/src/main/java/com/yapp/breake/presentation/login/LoginScreen.kt
@@ -1,28 +1,33 @@
 package com.yapp.breake.presentation.login
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.yapp.breake.core.auth.KakaoScreen
 import com.yapp.breake.core.designsystem.component.KakaoLoginButton
 import com.yapp.breake.core.designsystem.theme.BrakeTheme
 import com.yapp.breake.core.designsystem.theme.LocalPadding
 import com.yapp.breake.presentation.login.model.LoginEffect.NavigateToHome
 import com.yapp.breake.presentation.login.model.LoginEffect.NavigateToSignup
-import kotlinx.coroutines.launch
+import com.yapp.breake.presentation.login.model.LoginUiState
 
 @Composable
 internal fun LoginRoute(
@@ -31,29 +36,47 @@ internal fun LoginRoute(
 	onShowErrorSnackBar: (Throwable?) -> Unit,
 	viewModel: LoginViewModel = hiltViewModel(),
 ) {
-	val context = LocalContext.current
 	val padding = LocalPadding.current.screenPaddingHorizontal
+	val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
 	LaunchedEffect(true) {
-		launch {
-			viewModel.errorFlow.collect { onShowErrorSnackBar(it) }
-		}
-		launch {
-			viewModel.navigationFlow.collect { navigation ->
-				when (navigation) {
-					NavigateToHome -> navigateToHome()
-					NavigateToSignup -> navigateToSignup()
-				}
+		viewModel.errorFlow.collect { onShowErrorSnackBar(it) }
+	}
+
+	LaunchedEffect(true) {
+		viewModel.navigationFlow.collect { navigation ->
+			when (navigation) {
+				NavigateToHome -> navigateToHome()
+				NavigateToSignup -> navigateToSignup()
 			}
 		}
 	}
 
-	LoginScreen(
-		padding = padding,
-		onLoginClick = {
-			viewModel.loginWithKakao(context)
-		},
-	)
+	if (uiState == LoginUiState.LoginIdle) {
+		LoginScreen(
+			padding = padding,
+			onLoginClick = viewModel::loginWithKakao,
+		)
+	}
+
+	// 서버 응답 대기 시간이 1초가 넘어 로딩창 추가
+	if (uiState == LoginUiState.LoginLoading) {
+		Box(
+			modifier = Modifier.fillMaxSize().statusBarsPadding(),
+			contentAlignment = Alignment.Center,
+		) {
+			BackHandler { viewModel.loginCancel() }
+			CircularProgressIndicator(modifier = Modifier.fillMaxSize(0.2f))
+		}
+	}
+
+	if (uiState == LoginUiState.LoginOnWebView) {
+		KakaoScreen(
+			onBack = viewModel::loginCancel,
+			onAuthSuccess = viewModel::authSuccess,
+			onAuthError = viewModel::loginFailure,
+		)
+	}
 }
 
 @Composable
--- presentation/login/src/main/java/com/yapp/breake/presentation/login/LoginViewModel.kt
+++ presentation/login/src/main/java/com/yapp/breake/presentation/login/LoginViewModel.kt
@@ -5,9 +5,13 @@ import androidx.lifecycle.viewModelScope
 import com.yapp.breake.core.model.user.UserTokenStatus
 import com.yapp.breake.domain.usecase.LoginUseCase
 import com.yapp.breake.presentation.login.model.LoginEffect
+import com.yapp.breake.presentation.login.model.LoginUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -17,9 +21,64 @@ internal class LoginViewModel @Inject constructor(
 	private val loginUseCase: LoginUseCase,
 ) : ViewModel() {
 
+	private val _uiState = MutableStateFlow<LoginUiState>(LoginUiState.LoginIdle)
+	val uiState = _uiState.asStateFlow()
+
 	private val _errorFlow = MutableSharedFlow<Throwable>()
 	val errorFlow = _errorFlow.asSharedFlow()
 
 	private val _navigationFlow = MutableSharedFlow<LoginEffect>()
 	val navigationFlow = _navigationFlow.asSharedFlow()
+
+	private var loginJob: Job? = null
+
+	fun loginWithKakao() {
+		viewModelScope.launch {
+			_uiState.value = LoginUiState.LoginOnWebView
+		}
+	}
+
+	fun loginCancel() {
+		viewModelScope.launch {
+			loginJob?.cancel()
+			_uiState.value = LoginUiState.LoginIdle
+		}
+	}
+
+	fun authSuccess(authCode: String) {
+		loginJob?.cancel()
+		loginJob = viewModelScope.launch {
+			_uiState.value = LoginUiState.LoginLoading
+			loginUseCase(authCode) { throwable ->
+				_uiState.value = LoginUiState.LoginIdle
+				_errorFlow.emit(throwable)
+				return@loginUseCase
+			}.firstOrNull { result ->
+				when (result) {
+					UserTokenStatus.ACTIVE -> {
+						_uiState.value = LoginUiState.LoginIdle
+						_navigationFlow.emit(LoginEffect.NavigateToHome)
+					}
+
+					UserTokenStatus.HALF_SIGNUP -> {
+						_uiState.value = LoginUiState.LoginIdle
+						_navigationFlow.emit(LoginEffect.NavigateToSignup)
+					}
+
+					UserTokenStatus.INACTIVE -> {
+						_uiState.value = LoginUiState.LoginIdle
+						_errorFlow.emit(Throwable("서버에서 사용자 정보를 찾을 수 없습니다"))
+					}
+				}
+				true
+			}
+		}
+	}
+
+	fun loginFailure(throwable: Throwable) {
+		viewModelScope.launch {
+			_uiState.value = LoginUiState.LoginIdle
+			_errorFlow.emit(throwable)
+		}
+	}
 }
