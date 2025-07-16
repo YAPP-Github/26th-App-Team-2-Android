9e2e944f0b0dd54ad3dcfa5137877f772770793a
TaewoongR <iwoong98@kau.kr>
2025-07-16 23:08:28 +0900

    feat: LocalDataSource 선언 (#19)
    - 세가지 DataStore 를 위한 DataSource
    - DatastoreUserToken
    - DatastoreUserInfo
    - DatastoreAuthCode

---
+++ data/src/main/java/com/yapp/breake/data/local/UserLocalDataSource.kt
@@ -0,0 +1,49 @@
+package com.yapp.breake.data.local
+
+import androidx.datastore.core.DataStore
+import com.yapp.breake.core.datastore.model.DatastoreUserInfo
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+internal class UserLocalDataSource @Inject constructor(
+	private val userInfoDataStore: DataStore<DatastoreUserInfo>,
+) {
+	suspend fun updateNickname(
+		nickname: String,
+		onError: suspend (Throwable) -> Unit,
+	) {
+		userInfoDataStore.updateData {
+			it.copy(nickname = nickname)
+		}.runCatching {
+			// 성공적인 업데이트 후 아무 작업도 하지 않음
+		}.onFailure {
+			onError(Throwable("유저 닉네임 업데이트를 실패했습니다"))
+		}
+	}
+
+	suspend fun clearNickname(onError: suspend (Throwable) -> Unit) {
+		userInfoDataStore.updateData {
+			it.copy(nickname = null)
+		}.runCatching {
+			// 성공적인 업데이트 후 아무 작업도 하지 않음
+		}.onFailure {
+			onError(Throwable("유저 닉네임을 초기화하는데 실패했습니다"))
+		}
+	}
+
+	fun getNickname(onError: suspend (Throwable) -> Unit): Flow<String> = flow {
+		userInfoDataStore.data
+			.catch {
+				onError(Throwable("유저 닉네임을 가져오는데 실패했습니다"))
+			}
+			.collect { userInfo ->
+				userInfo.nickname?.let {
+					emit(userInfo.nickname!!)
+				} ?: run {
+					onError(Throwable("닉네임이 설정되어 있지 않습니다"))
+				}
+			}
+	}
+}
