75d320d140783ea9303d3a9fb73668936d9838d6
Jun <hky08067@naver.com>
2025-07-19 22:53:25 +0900

    chore: SetAlarmUsecase 정리 (#4)

--- domain/src/main/java/com/yapp/breake/domain/usecase/SetAlarmUsecase.kt
+++ domain/src/main/java/com/yapp/breake/domain/usecase/SetAlarmUsecase.kt
@@ -17,46 +17,17 @@ class SetAlarmUsecase @Inject constructor(
 		appGroupState: AppGroupState,
 		second: Int = 0,
 	): Result<Unit> {
-		return when (appGroupState) {
+
+		val (action, time) = when (appGroupState) {
 			AppGroupState.NeedSetting -> return Result.failure(IllegalStateException("알람을 예약하지 않는 상태입니다."))
-			AppGroupState.Using -> {
-				scheduleAlarm(
-					groupId = groupId,
-					second = second,
-					action = AlarmAction.ACTION_USING_FINISH,
-					appGroupState = appGroupState,
-				)
-			}
-
-			AppGroupState.Blocking -> {
-				scheduleAlarm(
-					groupId = groupId,
-					second = Constants.TEST_BLOCKING_TIME,
-					action = AlarmAction.ACTION_BLOCKING_FINISH,
-					appGroupState = appGroupState,
-				)
-			}
-
-			is AppGroupState.SnoozeBlocking -> {
-				scheduleAlarm(
-					groupId = groupId,
-					second = Constants.TEST_SNOOZE_TIME,
-					action = AlarmAction.ACTION_SNOOZE_FINISH,
-					appGroupState = appGroupState,
-				)
-			}
+			AppGroupState.Using -> AlarmAction.ACTION_USING_FINISH to second
+			AppGroupState.Blocking -> AlarmAction.ACTION_BLOCKING_FINISH to Constants.TEST_BLOCKING_TIME
+			is AppGroupState.SnoozeBlocking -> AlarmAction.ACTION_SNOOZE_FINISH to Constants.TEST_SNOOZE_TIME
 		}
-	}
 
-	private suspend fun scheduleAlarm(
-		groupId: Long,
-		second: Int,
-		action: AlarmAction,
-		appGroupState: AppGroupState,
-	): Result<Unit> {
 		return alarmScheduler.scheduleAlarm(
 			groupId = groupId,
-			second = second,
+			second = time,
 			action = action,
 		).onSuccess {
 			appGroupRepository.setAppGroupState(
