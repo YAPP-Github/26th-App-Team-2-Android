a82d6863739ff29c2a5a46b2eb075b9312469b9b
TaewoongR <iwoong98@kau.kr>
2025-07-26 16:46:23 +0900

    feat: 원형 이미지 컴포넌트 추가 (#32)

---
+++ core/designsystem/src/main/java/com/yapp/breake/core/designsystem/component/CircleImage.kt
@@ -0,0 +1,51 @@
+package com.yapp.breake.core.designsystem.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.skydoves.landscapist.coil.CoilImage
+import com.yapp.breake.core.designsystem.R
+
+@Composable
+fun CircleImage(
+	modifier: Modifier = Modifier,
+	imageUrl: String? = null,
+) {
+	CoilImage(
+		imageModel = { imageUrl },
+		modifier = modifier
+			.widthIn(min = 40.dp)
+			.aspectRatio(1f)
+			.clip(shape = CircleShape),
+		// 이미지 로딩 중 표시 내용
+		loading = {
+			Box(modifier = Modifier.matchParentSize()) {
+				CircularProgressIndicator(
+					modifier = Modifier.align(Alignment.Center),
+				)
+			}
+		},
+		// 이미지 요청 실패 시 표시 내용
+		failure = {
+			Box(
+				modifier = Modifier.matchParentSize(),
+				contentAlignment = Alignment.Center,
+			) {
+				Image(
+					painter = painterResource(id = R.drawable.img_loading_error),
+					contentDescription = null,
+					modifier = Modifier.matchParentSize(),
+				)
+			}
+		},
+	)
+}
