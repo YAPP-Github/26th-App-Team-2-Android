3a82b0183580ead51afb1bb9e3788024142f432b
TaewoongR <iwoong98@kau.kr>
2025-07-30 03:53:03 +0900

    feat: app table 삽입, 삭제 메서드 추가 (#34)

--- domain/src/main/java/com/yapp/breake/domain/repository/AppRepository.kt
+++ domain/src/main/java/com/yapp/breake/domain/repository/AppRepository.kt
@@ -5,7 +5,11 @@ import kotlinx.coroutines.flow.Flow
 
 interface AppRepository {
 
+	suspend fun insertApp(parentGroupId: Long, app: App)
+
 	fun observeApp(): Flow<List<App>>
 
 	suspend fun getAppGroupIdByPackage(packageName: String): Long?
+
+	suspend fun deleteAppByParentGroupId(parentGroupId: Long)
 }
