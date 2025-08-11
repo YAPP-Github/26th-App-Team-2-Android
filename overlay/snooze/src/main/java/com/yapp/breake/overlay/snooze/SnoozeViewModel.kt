2da20d133280b25d6c4fb163f75a1680f96cdddd
TaewoongR <iwoong98@kau.kr>
2025-08-12 04:23:41 +0900

    fix: 스누즈 Overlay 화면에서 블락 상태 전환 로직 추가 (#66)
    - 1. 버튼을 눌러 상태 전환
    - 2. 홈 화면(뒤로가기)로 이동시 상태 전환

--- overlay/snooze/src/main/java/com/yapp/breake/overlay/snooze/SnoozeViewModel.kt
+++ overlay/snooze/src/main/java/com/yapp/breake/overlay/snooze/SnoozeViewModel.kt
@@ -2,6 +2,8 @@ package com.yapp.breake.overlay.snooze
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.yapp.breake.core.model.app.AppGroupState
+import com.yapp.breake.domain.usecase.SetAlarmUseCase
 import com.yapp.breake.domain.usecase.SetSnoozeAlarmUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -12,6 +14,7 @@ import javax.inject.Inject
 @HiltViewModel
 internal class SnoozeViewModel @Inject constructor(
 	private val setSnoozeAlarmUsecase: SetSnoozeAlarmUseCase,
+	private val setAlarmUsecase: SetAlarmUseCase,
 ) : ViewModel() {
 
 	private val _toastEffect: MutableSharedFlow<String> = MutableSharedFlow()
@@ -26,6 +29,16 @@ internal class SnoozeViewModel @Inject constructor(
 			}.onFailure {
 				sendToastMessage("알람 설정에 실패했습니다. 정확한 알람 권한을 확인해주세요.")
 			}
+		}
+	}
+
+	fun setBlock(groupId: Long, appName: String) {
+		viewModelScope.launch {
+			setAlarmUsecase(
+				groupId = groupId,
+				appGroupState = AppGroupState.Blocking,
+				appName = appName,
+			)
 		}
 	}
 
