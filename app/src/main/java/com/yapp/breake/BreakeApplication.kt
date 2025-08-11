99fb73bafe3b0c2b756a911de0dd1448d58e90cf
TaewoongR <iwoong98@kau.kr>
2025-08-11 17:55:00 +0900

    chore: app 모듈 WorkManager 설정 (#66)
    - Application 에서 초기화
    - Work 라이브러리에 포함된 InitializationProvider 라이브러리 내부 설정

--- app/src/main/java/com/yapp/breake/BreakeApplication.kt
+++ app/src/main/java/com/yapp/breake/BreakeApplication.kt
@@ -2,11 +2,28 @@ package com.yapp.breake
 
 import android.app.Application
 import android.os.Build
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import androidx.work.WorkManager
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
-class BreakeApplication : Application() {
+class BreakeApplication : Application(), Configuration.Provider {
+
+	@Inject lateinit var workerFactory: HiltWorkerFactory
+
+	/**
+	 * WorkManager 설정을 제공하는 프로퍼티
+	 *
+	 * HiltWorker 2.1.0 이상 버전의 WorkManager 초기화 공식 방식
+	 */
+	override val workManagerConfiguration: Configuration
+		get() = Configuration.Builder()
+			.setWorkerFactory(workerFactory)
+			.build()
+
 	override fun onCreate() {
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
 			// API 31+ 기본 스플래시 사용
@@ -16,6 +33,9 @@ class BreakeApplication : Application() {
 			setTheme(R.style.Theme_Breake_Splash)
 			super.onCreate()
 		}
+
+		// WorkManager 초기화
+		WorkManager.initialize(this, workManagerConfiguration)
 
 		initTimber()
 	}
