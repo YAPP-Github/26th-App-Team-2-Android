73d21cb145567723ee3c7efa2b403a5e24ebca6c
TaewoongR <iwoong98@kau.kr>
2025-08-25 18:10:29 +0900

    feat: Google 로그인 기능 추가 (#81)

--- presentation/login/src/main/java/com/yapp/breake/presentation/login/LoginScreen.kt
+++ presentation/login/src/main/java/com/yapp/breake/presentation/login/LoginScreen.kt
@@ -1,6 +1,8 @@
 package com.yapp.breake.presentation.login
 
 import androidx.activity.compose.BackHandler
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -22,14 +24,17 @@ import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.yapp.breake.core.auth.KakaoScreen
-import com.yapp.breake.core.designsystem.component.KakaoLoginButton
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.common.api.ApiException
+import com.yapp.breake.core.auth.kakao.KakaoScreen
 import com.yapp.breake.core.designsystem.theme.BrakeTheme
 import com.yapp.breake.core.designsystem.theme.LocalPadding
 import com.yapp.breake.core.navigation.compositionlocal.LocalMainAction
 import com.yapp.breake.core.navigation.compositionlocal.LocalNavigatorAction
 import com.yapp.breake.core.navigation.compositionlocal.LocalNavigatorProvider
 import com.yapp.breake.core.ui.SnackBarState
+import com.yapp.breake.presentation.login.component.GoogleLoginButton
+import com.yapp.breake.presentation.login.component.KakaoLoginButton
 import com.yapp.breake.presentation.login.component.LoginNoticeText
 import com.yapp.breake.presentation.login.model.LoginNavState.NavigateToHome
 import com.yapp.breake.presentation.login.model.LoginNavState.NavigateToOnboarding
@@ -38,6 +43,7 @@ import com.yapp.breake.presentation.login.model.LoginNavState.NavigateToPrivacyP
 import com.yapp.breake.presentation.login.model.LoginNavState.NavigateToSignup
 import com.yapp.breake.presentation.login.model.LoginNavState.NavigateToTermsOfService
 import com.yapp.breake.presentation.login.model.LoginUiState
+import timber.log.Timber
 
 @Composable
 internal fun LoginRoute(viewModel: LoginViewModel = hiltViewModel()) {
@@ -51,7 +57,7 @@ internal fun LoginRoute(viewModel: LoginViewModel = hiltViewModel()) {
 	if (uiState == LoginUiState.LoginLoading) {
 		mainAction.OnShowLoading()
 		BackHandler {
-			viewModel.loginCancel()
+			viewModel.cancelLogin()
 		}
 	} else {
 		mainAction.OnFinishBackHandler()
@@ -86,18 +92,59 @@ internal fun LoginRoute(viewModel: LoginViewModel = hiltViewModel()) {
 		}
 	}
 
+	val authorizationLauncher = rememberLauncherForActivityResult(
+		contract = ActivityResultContracts.StartIntentSenderForResult(),
+	) { result ->
+		try {
+			// 결과에서 AuthorizationResult 추출
+			val authorizationResult = Identity.getAuthorizationClient(context)
+				.getAuthorizationResultFromIntent(result.data)
+
+			authorizationResult.serverAuthCode?.let { code ->
+				Timber.d("Google One Tap 로그인 성공, code: $code")
+				viewModel.loginWithGoogle(context = context, authCode = code)
+			} ?: run {
+				Timber.d("Google One Tap 로그인 취소 또는 실패")
+				viewModel.cancelGoogleAuthorization()
+			}
+		} catch (e: ApiException) {
+			when (e.statusCode) {
+				7 -> {
+					Timber.e("네트워크 문제로 Google 로그인에 실패했습니다")
+					viewModel.failGoogleAuthorization()
+				}
+				16 -> {
+					Timber.d("유저가 Google 로그인 창을 닫았습니다")
+					viewModel.cancelGoogleAuthorization()
+				}
+				else -> {
+					Timber.e(e, "Google 로그인 API 예외 발생")
+					viewModel.failGoogleAuthorization()
+				}
+			}
+		} catch (e: Exception) {
+			Timber.e(e, "Google One Tap 로그인 알 수 없는 예외 발생")
+			viewModel.failGoogleAuthorization()
+		}
+	}
+
 	LoginScreen(
 		padding = padding,
 		onPrivacyClick = viewModel::showPrivacyPolicy,
 		onTermsClick = viewModel::showTermsOfService,
-		onLoginClick = viewModel::loginWithKakao,
+		onGoogleLoginClick = {
+			viewModel.getGoogleAuthorization(context) { intent ->
+				authorizationLauncher.launch(intent)
+			}
+		},
+		onkakaoLoginClick = viewModel::getKakaoAuthorization,
 	)
 
 	if (uiState == LoginUiState.LoginOnWebView) {
 		KakaoScreen(
-			onBack = viewModel::authCancel,
-			onAuthSuccess = { viewModel.authSuccess(it, context) },
-			onAuthError = viewModel::loginFailure,
+			onBack = viewModel::cancelKakaoAuthorization,
+			onAuthSuccess = { viewModel.loginWithKakao(context = context, authCode = it) },
+			onAuthError = viewModel::failKakaoAuthorization,
 		)
 	}
 }
@@ -107,12 +154,13 @@ fun LoginScreen(
 	padding: Dp,
 	onPrivacyClick: () -> Unit,
 	onTermsClick: () -> Unit,
-	onLoginClick: () -> Unit,
+	onGoogleLoginClick: () -> Unit,
+	onkakaoLoginClick: () -> Unit,
 ) {
 	ConstraintLayout(
 		modifier = Modifier.fillMaxSize(),
 	) {
-		val (title, notice, loginButton) = createRefs()
+		val (title, notice, googleLoginButton, kakaoLoginButton) = createRefs()
 
 		Box(
 			modifier = Modifier
@@ -142,13 +190,28 @@ fun LoginScreen(
 		LoginNoticeText(
 			modifier = Modifier
 				.constrainAs(notice) {
-					bottom.linkTo(loginButton.top, margin = 20.dp)
+					top.linkTo(title.bottom)
+					bottom.linkTo(googleLoginButton.top, margin = 20.dp)
 					start.linkTo(parent.start)
 					end.linkTo(parent.end)
+					// top, bottom 과 linkTo 관계가 설정되어 있을 때, 해당 컴포넌트 y 위치를 바텀(1f)으로 조정
+					verticalBias = 1f
 				}
 				.padding(horizontal = padding),
 			onPrivacyClick = onPrivacyClick,
 			onTermsClick = onTermsClick,
+		)
+
+		GoogleLoginButton(
+			modifier = Modifier
+				.padding(horizontal = padding)
+				.widthIn(max = 400.dp)
+				.constrainAs(googleLoginButton) {
+					bottom.linkTo(kakaoLoginButton.top, margin = 12.dp)
+					start.linkTo(parent.start)
+					end.linkTo(parent.end)
+				},
+			onClick = onGoogleLoginClick,
 		)
 
 		KakaoLoginButton(
@@ -157,13 +220,12 @@ fun LoginScreen(
 				.padding(horizontal = padding)
 				.padding(bottom = 24.dp)
 				.widthIn(max = 400.dp)
-				.constrainAs(loginButton) {
+				.constrainAs(kakaoLoginButton) {
 					bottom.linkTo(parent.bottom)
 					start.linkTo(parent.start)
 					end.linkTo(parent.end)
 				},
-			text = "카카오 로그인",
-			onClick = onLoginClick,
+			onClick = onkakaoLoginClick,
 		)
 	}
 }
