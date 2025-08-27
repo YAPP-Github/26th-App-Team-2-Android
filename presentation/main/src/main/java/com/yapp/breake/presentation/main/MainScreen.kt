240e69642afbeea81dbff686512d74258c2d9914
TaewoongR <iwoong98@kau.kr>
2025-08-27 09:12:28 +0900

    fix: 최상위 Composalbe 수정 (#85)
    - 스낵바 높이 위치 조정
    - 화면에 따른 바텀 네비게이션 바 표시 처리

--- presentation/main/src/main/java/com/yapp/breake/presentation/main/MainScreen.kt
+++ presentation/main/src/main/java/com/yapp/breake/presentation/main/MainScreen.kt
@@ -6,13 +6,17 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.yapp.breake.core.designsystem.component.BrakeSnackbar
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.yapp.breake.core.navigation.route.InitialRoute
+import com.yapp.breake.core.navigation.route.MainTabRoute
+import com.yapp.breake.presentation.main.component.BrakeSnackbar
+import com.yapp.breake.presentation.main.component.BrakeSnackbarHostState
+import com.yapp.breake.presentation.main.component.BrakeSnackbarHost
 import com.yapp.breake.presentation.main.navigation.MainBottomNavBar
 import com.yapp.breake.presentation.main.navigation.MainNavHost
 import com.yapp.breake.presentation.main.navigation.MainNavigator
@@ -23,7 +27,7 @@ import kotlinx.collections.immutable.toPersistentList
 internal fun MainScreen(
 	navigator: MainNavigator,
 	onChangeDarkTheme: (Boolean) -> Unit,
-	snackBarHostState: SnackbarHostState,
+	snackBarHostState: BrakeSnackbarHostState,
 ) {
 	MainScreenContent(
 		navigator = navigator,
@@ -36,9 +40,13 @@ internal fun MainScreen(
 private fun MainScreenContent(
 	navigator: MainNavigator,
 	onChangeDarkTheme: (Boolean) -> Unit,
-	snackBarHostState: SnackbarHostState,
+	snackBarHostState: BrakeSnackbarHostState,
 	modifier: Modifier = Modifier,
 ) {
+	// 1. 현재 화면에 따라 실시간 스낵바 위치 조정을 위한 Route 구독
+	// 2. MainTabRoute 화면일 때 하단 네비게이션 바를 띄우고, 그 외는 안띄우기 위한 Route 구독
+	val currentRoute by navigator.currentRoute.collectAsStateWithLifecycle()
+
 	Scaffold(
 		modifier = modifier,
 		content = { padding ->
@@ -55,27 +63,42 @@ private fun MainScreenContent(
 					.fillMaxWidth()
 					.padding(bottom = 34.dp)
 					.wrapContentWidth(Alignment.CenterHorizontally),
-				visible = navigator.shouldShowBottomBar(),
+				visible = currentRoute is MainTabRoute,
 				tabs = MainTab.entries.toPersistentList(),
-				currentTab = navigator.currentTab,
+				currentTab = when (currentRoute) {
+					is MainTabRoute.Home -> MainTab.HOME
+					is MainTabRoute.Report -> MainTab.REPORT
+					is MainTabRoute.Setting -> MainTab.SETTING
+					else -> null
+				},
 				onTabSelected = navigator::navigate,
 			)
 		},
 		contentWindowInsets = WindowInsets(0.dp),
 		snackbarHost = {
-			SnackbarHost(
+			BrakeSnackbarHost(
 				hostState = snackBarHostState,
 				snackbar = { snackbarData ->
 					BrakeSnackbar(
 						snackbarData = snackbarData,
 					)
 				},
-				modifier = if (!navigator.shouldShowBottomBar()) {
-					// 시스템 하단 패딩 + 버튼 패딩
-					Modifier.navigationBarsPadding().padding(bottom = 80.dp)
-				} else {
-					Modifier
-				},
+				// 현재 화면에 따라 스낵바 y 축 위치 조정
+				modifier = Modifier.then(
+					when (currentRoute) {
+						is MainTabRoute -> Modifier.padding(bottom = 0.dp)
+
+						InitialRoute.Login ->
+							Modifier
+								.navigationBarsPadding()
+								.padding(bottom = 190.dp)
+
+						else ->
+							Modifier
+								.navigationBarsPadding()
+								.padding(bottom = 80.dp)
+					},
+				),
 			)
 		},
 	)
