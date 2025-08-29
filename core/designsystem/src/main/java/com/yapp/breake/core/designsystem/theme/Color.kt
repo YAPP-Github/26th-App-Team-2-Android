5a3d91b8f0f9114650c7597d6df2a910d177c310
TaewoongR <iwoong98@kau.kr>
2025-08-29 18:15:00 +0900

    fix: 홈 화면 색상 수정 (#89)

--- core/designsystem/src/main/java/com/yapp/breake/core/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/yapp/breake/core/designsystem/theme/Color.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.graphics.Color
 
 val White = Color(0xFFF2F2F2)
 
+val Gray950 = Color(0xFF0B0C0D)
 val Gray900 = Color(0xFF1E2023)
 val Gray850 = Color(0xFF292C31)
 val Gray800 = Color(0xFF3A3D45)
@@ -20,7 +21,7 @@ val InsightBlue = Color(0xFF2684FF)
 val BrakeYellow = Color(0xFFF2FF5E)
 val ButtonYellow = Color(0xFFF3FF6E)
 val KakaoYellow = Color(0xFFFFE502)
-val Error = Color(0xFFE75B5D)
+val Error = Color(0xFFFF5762)
 
 val Red = Color(0xFFFF4343)
 val Red2 = Color(0xFFFF5656)
