f225273459a608fc79343d57b40b11eb15572877
TaewoongR <iwoong98@kau.kr>
2025-08-30 03:05:00 +0900

    fix: 그룹 사용중, 사용금지 상태 화면 통폐합 (#89)
    - BlockingScreen + UsingScreen -> TickingScreen

---
+++ presentation/home/src/main/java/com/yapp/breake/presentation/home/component/BlockingAppGroup.kt
@@ -0,0 +1,79 @@
+package com.yapp.breake.presentation.home.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.yapp.breake.core.designsystem.component.VerticalSpacer
+import com.yapp.breake.core.designsystem.theme.BrakeTheme
+import com.yapp.breake.core.model.app.AppGroup
+import com.yapp.breake.presentation.home.R
+
+@Composable
+internal fun BlockingAppGroup(
+	appGroup: AppGroup,
+	onEditClick: () -> Unit,
+	modifier: Modifier = Modifier,
+	innerModifier: Modifier = Modifier,
+) {
+	AppGroupBox(
+		modifier = modifier,
+		innerModifier = innerModifier,
+	) {
+		AppGroupItemContent(
+			appGroup = appGroup,
+			clickable = false,
+			onEditClick = onEditClick,
+			isDimmed = true,
+		)
+		VerticalSpacer(16.dp)
+		Column(
+			horizontalAlignment = Alignment.CenterHorizontally,
+		) {
+			ProgressTime(
+				startTime = appGroup.startTime,
+				endTime = appGroup.endTime,
+				minuteTextStyle = BrakeTheme.typography.subtitle14B,
+				startColor = Color(0xFF8E97B0),
+				endColor = Color(0xFFF0F4FF),
+				textBottomPadding = 0,
+				modifier = Modifier.fillMaxWidth(),
+			) {
+				Image(
+					painter = painterResource(id = R.drawable.img_lock),
+					contentDescription = "Lock Icon",
+					contentScale = ContentScale.FillWidth,
+					modifier = Modifier
+						.width(110.dp)
+						.wrapContentHeight(),
+				)
+			}
+		}
+		VerticalSpacer(62.dp)
+	}
+}
+
+@Preview
+@Composable
+private fun BlockingScreenPreview() {
+	BrakeTheme {
+		BlockingAppGroup(
+			appGroup = AppGroup.sample,
+			onEditClick = { /* TODO: Handle edit click */ },
+			modifier = Modifier
+				.fillMaxWidth()
+				.padding(horizontal = 16.dp),
+			innerModifier = Modifier.padding(16.dp),
+		)
+	}
+}
---
+++ presentation/home/src/main/java/com/yapp/breake/presentation/home/component/UsingAppGroup.kt
@@ -0,0 +1,70 @@
+package com.yapp.breake.presentation.home.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.yapp.breake.core.designsystem.component.VerticalSpacer
+import com.yapp.breake.core.designsystem.theme.BrakeTheme
+import com.yapp.breake.core.designsystem.theme.Gray400
+import com.yapp.breake.core.model.app.AppGroup
+import com.yapp.breake.presentation.home.R
+
+@Composable
+internal fun UsingAppGroup(
+	appGroup: AppGroup,
+	onEditClick: () -> Unit,
+	onStopClick: () -> Unit,
+	modifier: Modifier = Modifier,
+	innerModifier: Modifier = Modifier,
+) {
+	AppGroupBox(
+		modifier = modifier,
+		innerModifier = innerModifier,
+	) {
+		AppGroupItemContent(
+			appGroup = appGroup,
+			clickable = false,
+			onEditClick = onEditClick,
+		)
+		VerticalSpacer(16.dp)
+		Column(
+			horizontalAlignment = Alignment.CenterHorizontally,
+		) {
+			ProgressTime(
+				startTime = appGroup.startTime,
+				endTime = appGroup.endTime,
+				modifier = Modifier.fillMaxWidth(),
+			) {
+				Text(
+					text = stringResource(R.string.remaining_usage_time),
+					style = BrakeTheme.typography.body16M,
+					color = Gray400,
+				)
+			}
+			VerticalSpacer(10.dp)
+			StopButton(
+				onStopClick = onStopClick,
+				modifier = Modifier,
+			)
+		}
+		VerticalSpacer(10.dp)
+	}
+}
+
+@Preview
+@Composable
+private fun UsingAppGroupPreview() {
+	BrakeTheme {
+		UsingAppGroup(
+			appGroup = AppGroup.sample,
+			onEditClick = {},
+			onStopClick = {},
+		)
+	}
+}
