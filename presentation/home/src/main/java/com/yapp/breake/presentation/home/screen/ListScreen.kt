04837c965878b51bcdc55228b4926d353c4938bb
TaewoongR <iwoong98@kau.kr>
2025-09-01 09:30:00 +0900

    fix: 그룹 사용전 상태창 수정 (#89)

--- presentation/home/src/main/java/com/yapp/breake/presentation/home/screen/ListScreen.kt
+++ presentation/home/src/main/java/com/yapp/breake/presentation/home/screen/ListScreen.kt
@@ -1,23 +1,15 @@
 package com.yapp.breake.presentation.home.screen
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.LocalOverscrollFactory
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.ScrollableDefaults
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
@@ -28,8 +20,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.derivedStateOf
@@ -40,18 +30,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.yapp.breake.core.designsystem.component.HorizontalSpacer
 import com.yapp.breake.core.designsystem.component.VerticalSpacer
+import com.yapp.breake.core.designsystem.theme.AppItemGradient
 import com.yapp.breake.core.designsystem.theme.BrakeTheme
-import com.yapp.breake.core.designsystem.theme.Gray100
-import com.yapp.breake.core.designsystem.theme.Gray200
-import com.yapp.breake.core.designsystem.theme.Gray900
 import com.yapp.breake.core.model.app.AppGroup
 import com.yapp.breake.presentation.home.R
 import com.yapp.breake.presentation.home.component.AppGroupItem
+import com.yapp.breake.presentation.home.component.AppGroupSubtitle
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -60,151 +48,99 @@ internal fun ListScreen(
 	onEditClick: (AppGroup) -> Unit,
 	onAddClick: () -> Unit,
 ) {
-	val listState = rememberLazyListState()
-	val showTitle by remember {
+	val needSettingState = rememberLazyListState()
+	// 현재 보이는 아이템의 index 추적
+	val currentOffset by remember {
 		derivedStateOf {
-			listState.firstVisibleItemIndex > 0
+			needSettingState.firstVisibleItemScrollOffset
 		}
 	}
-	val alpha by animateFloatAsState(
-		targetValue = if (showTitle) 1f else 0f,
-		animationSpec = tween(20, easing = LinearEasing),
-		label = "appbarAlpha",
-	)
-	val container = Gray900.copy(alpha = alpha)
-	val headerKey = "groupsHeader"
+	val currentIndex by remember {
+		derivedStateOf {
+			// 첫 아이템 패딩 고려
+			val baseIndex = when (val lazyIndex = needSettingState.firstVisibleItemIndex) {
+				0 -> lazyIndex + 1
+				else -> lazyIndex
+			}
+			val adjustedIndex = when {
+				currentOffset >= 270 -> baseIndex + 1
+				else -> baseIndex
+			}
+			adjustedIndex
+		}
+	}
 
 	Scaffold(
-		topBar = {
-			TopAppBar(
-				title = {
-					AnimatedVisibility(
-						visible = showTitle,
-						modifier = Modifier.fillMaxWidth(),
-						enter = fadeIn(),
-						exit = fadeOut(),
+		content = { paddingValue ->
+			Column(
+				modifier = Modifier
+					.fillMaxWidth()
+					.padding(paddingValue),
+				horizontalAlignment = Alignment.CenterHorizontally,
+			) {
+				Image(
+					painter = painterResource(id = R.drawable.img_home_list),
+					contentDescription = null,
+					modifier = Modifier.fillMaxWidth(),
+					contentScale = ContentScale.FillWidth,
+				)
+
+				Row(
+					verticalAlignment = Alignment.CenterVertically,
+					modifier = Modifier
+						.fillMaxWidth()
+						.padding(horizontal = 32.dp),
+				) {
+					Text(
+						text = stringResource(R.string.group),
+						style = BrakeTheme.typography.subtitle22SB,
+						color = MaterialTheme.colorScheme.onSurface,
+					)
+
+					HorizontalSpacer(1f)
+
+					IconButton(
+						onClick = onAddClick,
+						modifier = Modifier.align(Alignment.CenterVertically),
 					) {
-						Text(
-							text = stringResource(R.string.list_screen_title),
-							style = BrakeTheme.typography.subtitle16SB,
-							color = Gray100,
-							textAlign = TextAlign.Center,
-						)
-					}
-				},
-				modifier = Modifier.animateContentSize(),
-				navigationIcon = {
-					// 공간만 차지하는 네비게이션 아이콘
-					HorizontalSpacer(48.dp)
-				},
-				actions = {
-					IconButton(onClick = onAddClick) {
 						Icon(
 							imageVector = Icons.Default.Add,
 							contentDescription = stringResource(R.string.add_button_content_description),
 						)
 					}
-				},
-				colors = TopAppBarDefaults.topAppBarColors(
-					containerColor = container,
-					scrolledContainerColor = container,
-					navigationIconContentColor = Gray100,
-					titleContentColor = Gray100,
-					actionIconContentColor = Gray100,
-				),
-			)
-		},
-		content = { innerPadding ->
-			CompositionLocalProvider(LocalOverscrollFactory provides null) {
-				LazyColumn(
-					modifier = Modifier
-						.fillMaxSize()
-						.padding(bottom = 16.dp),
-					state = listState,
-					horizontalAlignment = Alignment.CenterHorizontally,
-				) {
-					item {
-						Column(
-							modifier = Modifier
-								.fillMaxWidth()
-								.padding(horizontal = 16.dp),
-							horizontalAlignment = Alignment.CenterHorizontally,
-						) {
-							Image(
-								painter = painterResource(id = R.drawable.img_home_list),
-								contentDescription = null,
-								modifier = Modifier.fillMaxWidth(),
-								contentScale = ContentScale.FillWidth,
-							)
-							VerticalSpacer(12.dp)
-							Text(
-								text = stringResource(R.string.list_screen_description),
-								style = BrakeTheme.typography.subtitle22SB,
-								color = Gray100,
-								textAlign = TextAlign.Center,
-							)
-						}
-					}
+				}
 
-					stickyHeader(key = headerKey) {
-						val isPinned by remember {
-							derivedStateOf {
-								val info = listState.layoutInfo.visibleItemsInfo
-									.firstOrNull { it.key == headerKey }
-								info?.offset == 0
-							}
-						}
+				VerticalSpacer(18.dp)
 
-						// 핀일 때만 AppBar 높이 적용, 아니면 0
-						val topInset = innerPadding.calculateTopPadding()
-						val spacer by animateDpAsState(
-							targetValue = if (isPinned) topInset else 36.dp,
-							animationSpec = tween(500, easing = LinearOutSlowInEasing),
-							label = "HeaderTopInset",
-						)
-						Column(
-							modifier = Modifier.background(container),
-						) {
-							VerticalSpacer(spacer)
-							Row(
-								verticalAlignment = Alignment.Bottom,
+				AppGroupSubtitle(
+					modifier = Modifier.padding(horizontal = 28.dp),
+					titleResId = R.string.group_title_need_setting,
+					currentIndex = currentIndex,
+					totalCount = appGroups.size,
+				)
+
+				VerticalSpacer(12.dp)
+
+				CompositionLocalProvider(LocalOverscrollFactory provides null) {
+					LazyRow(
+						flingBehavior = ScrollableDefaults.flingBehavior(),
+						state = needSettingState,
+					) {
+						item { HorizontalSpacer(28.dp) }
+						itemsIndexed(appGroups) { index, appGroup ->
+							AppGroupItem(
+								appGroup = appGroup,
+								onEditClick = { onEditClick(appGroup) },
 								modifier = Modifier
-									.fillMaxWidth()
-									.padding(top = 4.dp, bottom = 16.dp)
-									.padding(horizontal = 24.dp),
-							) {
-								Text(
-									text = stringResource(R.string.group),
-									style = BrakeTheme.typography.subtitle22SB,
-									color = MaterialTheme.colorScheme.onSurface,
-								)
-								HorizontalSpacer(1f)
-								Text(
-									text = stringResource(
-										R.string.group_count_format,
-										appGroups.size,
-									),
-									style = BrakeTheme.typography.body12M,
-									color = Gray200,
-								)
+									.width(214.dp)
+									.background(AppItemGradient),
+								innerModifier = Modifier.padding(16.dp),
+							)
+							if (index != appGroups.lastIndex) {
+								HorizontalSpacer(12.dp)
 							}
 						}
-					}
-
-					itemsIndexed(
-						appGroups,
-						key = { _, appGroup -> appGroup.id },
-					) { index, appGroup ->
-						AppGroupItem(
-							appGroup = appGroup,
-							onEditClick = { onEditClick(appGroup) },
-							modifier = Modifier
-								.fillMaxWidth()
-								.padding(horizontal = 16.dp),
-						)
-						if (index != appGroups.lastIndex) {
-							VerticalSpacer(12.dp)
-						}
+						item { HorizontalSpacer(28.dp) }
 					}
 				}
 			}
