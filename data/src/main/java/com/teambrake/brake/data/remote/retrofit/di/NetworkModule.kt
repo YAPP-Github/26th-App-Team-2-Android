b94fd0e83a112bce84ba26af25d13b6fd2f638e2
TaewoongR <iwoong98@kau.kr>
2025-11-18 11:16:10 +0900

    fix: retrofit timeout 시간초 변경 (#109)
    - 30초 -> 3초 변경
    - 적은 횟수로 장기간 대기시 예외 발생 및 종료

--- data/src/main/java/com/teambrake/brake/data/remote/retrofit/di/NetworkModule.kt
+++ data/src/main/java/com/teambrake/brake/data/remote/retrofit/di/NetworkModule.kt
@@ -39,9 +39,9 @@ internal object NetworkModule {
 			.apply {
 				interceptors.get().forEach(::addInterceptor)
 			}
-			.connectTimeout(10, TimeUnit.SECONDS)
-			.writeTimeout(30, TimeUnit.SECONDS)
-			.readTimeout(30, TimeUnit.SECONDS)
+			.connectTimeout(1, TimeUnit.SECONDS)
+			.writeTimeout(2, TimeUnit.SECONDS)
+			.readTimeout(2, TimeUnit.SECONDS)
 			.addInterceptor(RetryTimeoutInterceptor(maxRetries = 1))
 			.addInterceptor(
 				RefreshTokenInterceptor(
