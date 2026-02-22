418a26e0538ea640fa308bd476ca1cc58c4f8a38
Jun <hky08067@naver.com>
2026-02-22 15:40:23 +0900

    chore: update dependencies for Navigation 3 and target SDK version

--- build-logic/src/main/kotlin/brake.android.feature.gradle.kts
+++ build-logic/src/main/kotlin/brake.android.feature.gradle.kts
@@ -32,9 +32,7 @@ dependencies {
 
     val libs = project.extensions.libs
     implementation(libs.findLibrary("hilt.navigation.compose").get())
-    implementation(libs.findLibrary("androidx.compose.navigation").get())
     implementation(libs.findLibrary("androidx.compose.material.icon").get())
-    androidTestImplementation(libs.findLibrary("androidx.compose.navigation.test").get())
 
     implementation(libs.findLibrary("androidx.lifecycle.viewModelCompose").get())
     implementation(libs.findLibrary("androidx.lifecycle.runtimeCompose").get())
